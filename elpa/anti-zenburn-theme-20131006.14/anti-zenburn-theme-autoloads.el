ab4e1d8787e9ec60c88aed90bffb58a891cda9c4
Nikita <d6e6v9@live.com>
2013-10-10 02:38:56 +0400

    add anti-zenburn and keys

---
+++ elpa/anti-zenburn-theme-20131006.14/anti-zenburn-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; anti-zenburn-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "anti-zenburn-theme" "anti-zenburn-theme.el"
+;;;;;;  (21077 55657 0 0))
+;;; Generated autoloads from anti-zenburn-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("anti-zenburn-theme-pkg.el") (21077 55658
+;;;;;;  18000 0))
+
+;;;***
+
+(provide 'anti-zenburn-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; anti-zenburn-theme-autoloads.el ends here
---
+++ elpa/anti-zenburn-theme-20131006.14/anti-zenburn-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "anti-zenburn-theme" "20131006.14" "Low-contrast Zenburn-inverted theme [github]" (quote nil))
